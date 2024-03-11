b68471cbee6efd43cefc98a4fb94c88ba20950ae
Kurtis Streutker <kurtisstreutker@gmail.com>
2024-03-10 20:32:43 -0400

    refactor

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,19 +1,39 @@
 import Reserve from "@/components/reserve";
 import { LSTS, STABLES } from "@/utils/consts";
-import { LendingMarket } from "@/utils/models";
-import { Box, Stack, VStack } from "@chakra-ui/react";
+import { Asset, LendingMarket } from "@/utils/models";
+import {
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  Heading,
+  Stack,
+  StackDivider,
+  VStack,
+} from "@chakra-ui/react";
+
+const groups: [string, Asset[]][] = [
+  ["Stables", STABLES],
+  ["SOL and LSTs", LSTS],
+];
 
 export default function Home() {
   return (
     <main>
       <Stack p="1rem" spacing="1rem">
-        <Box fontSize="xl">Stables</Box>
-        {STABLES.map((s) => (
-          <Reserve asset={s} lendingMarket={LendingMarket.KAMINO} />
-        ))}
-        <Box fontSize="xl">SOL and LSTs</Box>
-        {LSTS.map((a) => (
-          <Reserve asset={a} lendingMarket={LendingMarket.KAMINO} />
+        {groups.map(([group, assets]) => (
+          <Card>
+            <CardBody>
+              <CardHeader>
+                <Heading size="md">{group}</Heading>
+              </CardHeader>
+              <Stack spacing="1rem" divider={<StackDivider />}>
+                {assets.map((s) => (
+                  <Reserve asset={s} lendingMarket={LendingMarket.KAMINO} />
+                ))}
+              </Stack>
+            </CardBody>
+          </Card>
         ))}
       </Stack>
     </main>
