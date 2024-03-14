70eaa01a9001e2977229fae3e260a5d8859fef9c
Kurtis Streutker <kurtisstreutker@gmail.com>
2024-03-13 20:15:35 -0400

    init reserves

--- cli/actions.ts
+++ cli/actions.ts
@@ -3,13 +3,19 @@ import { Command } from "commander";
 import {
   Transaction,
   Keypair,
-  sendAndConfirmTransaction as sendTx,
+  SystemProgram,
+  PublicKey,
 } from "@solana/web3.js";
 import fs from "fs";
 import { keys } from "../keys";
 import { keyPairFromB58 } from "../tests/utils";
 import { ShyftSdk, Network } from "@shyft-to/js";
 import { createWritePythPriceInstruction } from "../clients/mock-pyth/src";
+import {
+  PROGRAM_ID as KLEND_PROGRAM_ID,
+  createInitLendingMarketInstruction,
+  createInitReserveInstruction,
+} from "../clients/klend/src";
 
 const defaultKeypairLocation = "/home/sc4recoin/.config/solana/id.json";
 const ASSETS = ["USDC", "USDT", "SOL", "JitoSOL", "mSOL", "bSOL"];
@@ -37,14 +43,8 @@ cli
   .argument("<ticker>", "ticker of asset")
   .argument("<price>", "price in base")
   .argument("<decimals>", "price decimals")
-  .option(
-    "-kp, --keypair <keypair>",
-    "location of keypair",
-    defaultKeypairLocation
-  )
-  .action(async (ticker, price, decimals, optional) => {
-    const wallet = loadKeypair(optional.keypair);
-
+  .action(async (ticker, price, decimals) => {
+    const wallet = loadKeypair(defaultKeypairLocation);
     const oracle = keyPairFromB58(keys.oracles[ticker]);
 
     const ix = createWritePythPriceInstruction(
@@ -69,10 +69,148 @@ cli
     console.log({ signature });
   });
 
+cli.command("initLendingMarket").action(async () => {
+  const wallet = loadKeypair(defaultKeypairLocation);
+  const lendingMarket = keyPairFromB58(keys.lendingMarket);
+
+  const [lendingMarketAuthority] = PublicKey.findProgramAddressSync(
+    [Buffer.from("lma"), lendingMarket.publicKey.toBuffer()],
+    KLEND_PROGRAM_ID
+  );
+
+  const quoteCurrency = Array(32).fill(0);
+  Buffer.from("USD").forEach((b, i) => (quoteCurrency[i] = b));
+
+  const minBalance = await shyft.connection.getMinimumBalanceForRentExemption(
+    4656 + 8
+  );
+  const createMarketAccountTx = SystemProgram.createAccount({
+    fromPubkey: wallet.publicKey,
+    newAccountPubkey: lendingMarket.publicKey,
+    lamports: minBalance,
+    space: 4656 + 8,
+    programId: KLEND_PROGRAM_ID,
+  });
+
+  let tx = new Transaction().add(createMarketAccountTx);
+  let signature = await shyft.connection.sendTransaction(tx, [
+    wallet,
+    lendingMarket,
+  ]);
+  console.log({ signature });
+  await sleep(5000);
+
+  tx = new Transaction().add(
+    createInitLendingMarketInstruction(
+      {
+        lendingMarketOwner: wallet.publicKey,
+        lendingMarket: lendingMarket.publicKey,
+        lendingMarketAuthority,
+      },
+      {
+        quoteCurrency,
+      }
+    )
+  );
+
+  signature = await shyft.connection.sendTransaction(tx, [wallet]);
+  console.log({ signature });
+});
+
+cli
+  .command("initReserve")
+  .argument("<ticker>", "ticker of asset")
+  .action(async (ticker) => {
+    const wallet = loadKeypair(defaultKeypairLocation);
+    const lendingMarket = keyPairFromB58(keys.lendingMarket);
+    const mint = keyPairFromB58(keys.mints[ticker]);
+    const reserve = keyPairFromB58(keys.reserves[ticker]);
+
+    const minBalance = await shyft.connection.getMinimumBalanceForRentExemption(
+      8616 + 8
+    );
+    const createReserveAccountTx = SystemProgram.createAccount({
+      fromPubkey: wallet.publicKey,
+      newAccountPubkey: reserve.publicKey,
+      lamports: minBalance,
+      space: 8616 + 8,
+      programId: KLEND_PROGRAM_ID,
+    });
+
+    let tx = new Transaction().add(createReserveAccountTx);
+    let signature = await shyft.connection.sendTransaction(tx, [
+      wallet,
+      reserve,
+    ]);
+    console.log({ signature });
+    await sleep(10000);
+
+    const [lendingMarketAuthority] = PublicKey.findProgramAddressSync(
+      [Buffer.from("lma"), lendingMarket.publicKey.toBuffer()],
+      KLEND_PROGRAM_ID
+    );
+
+    const [feeReceiver] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("fee_receiver"),
+        lendingMarket.publicKey.toBuffer(),
+        mint.publicKey.toBuffer(),
+      ],
+      KLEND_PROGRAM_ID
+    );
+    const [reserveLiquiditySupply] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("reserve_liq_supply"),
+        lendingMarket.publicKey.toBuffer(),
+        mint.publicKey.toBuffer(),
+      ],
+      KLEND_PROGRAM_ID
+    );
+    const [reserveCollateralMint] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("reserve_coll_mint"),
+        lendingMarket.publicKey.toBuffer(),
+        mint.publicKey.toBuffer(),
+      ],
+      KLEND_PROGRAM_ID
+    );
+    const [reserveCollateralSupply] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("reserve_coll_supply"),
+        lendingMarket.publicKey.toBuffer(),
+        mint.publicKey.toBuffer(),
+      ],
+      KLEND_PROGRAM_ID
+    );
+
+    let txInit = new Transaction().add(
+      createInitReserveInstruction({
+        lendingMarketOwner: wallet.publicKey,
+        lendingMarket: lendingMarket.publicKey,
+        lendingMarketAuthority,
+        reserve: reserve.publicKey,
+        reserveLiquidityMint: mint.publicKey,
+        feeReceiver,
+        reserveLiquiditySupply,
+        reserveCollateralMint,
+        reserveCollateralSupply,
+      })
+    );
+
+    let signatureInit = await shyft.connection.sendTransaction(txInit, [
+      wallet,
+    ]);
+    console.log({ signature: signatureInit });
+  });
+
 const loadKeypair = (filename: string) => {
   const walletKey = JSON.parse(fs.readFileSync(filename, "utf-8"));
   let secretKey = Uint8Array.from(walletKey as number[]);
   return Keypair.fromSecretKey(secretKey);
 };
+
+function sleep(millis) {
+  return new Promise((resolve) => setTimeout(resolve, millis));
+}
 
 cli.parse(process.argv);
--- tests/accounts.ts
+++ tests/accounts.ts
@@ -1,8 +1,14 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from '@coral-xyz/anchor';
-import { keys } from '../keys'
-import { KaminoLending } from '../target/types/kamino_lending';
-import { PublicKey, Connection, Keypair, Signer, AccountMeta } from "@solana/web3.js";
+import { Program } from "@coral-xyz/anchor";
+import { keys } from "../keys";
+import { KaminoLending } from "../target/types/kamino_lending";
+import {
+  PublicKey,
+  Connection,
+  Keypair,
+  Signer,
+  AccountMeta,
+} from "@solana/web3.js";
 import { keyPairFromB58 } from "./utils";
 import { Bestlend } from "../target/types/bestlend";
 import { getOrCreateAssociatedTokenAccount } from "@solana/spl-token";
@@ -12,205 +18,228 @@ import { DummySwap } from "../target/types/dummy_swap";
 const program = anchor.workspace.Bestlend as Program<Bestlend>;
 const klend = anchor.workspace.KaminoLending as Program<KaminoLending>;
 const dummySwap = anchor.workspace.DummySwap as Program<DummySwap>;
-const lendingMarket = keyPairFromB58(keys.lendingMarket)
+const lendingMarket = keyPairFromB58(keys.lendingMarket);
 
 const [lendingMarketAuthority] = PublicKey.findProgramAddressSync(
-    [Buffer.from("lma"), lendingMarket.publicKey.toBuffer()],
-    klend.programId
+  [Buffer.from("lma"), lendingMarket.publicKey.toBuffer()],
+  klend.programId
 );
 
 const reservePDAs = (mint: PublicKey) => {
-    const [feeReceiver] = PublicKey.findProgramAddressSync(
-        [
-            Buffer.from("fee_receiver"),
-            lendingMarket.publicKey.toBuffer(),
-            mint.toBuffer(),
-        ],
-        klend.programId
-    );
-    const [reserveLiquiditySupply] = PublicKey.findProgramAddressSync(
-        [
-            Buffer.from("reserve_liq_supply"),
-            lendingMarket.publicKey.toBuffer(),
-            mint.toBuffer(),
-        ],
-        klend.programId
-    );
-    const [reserveCollateralMint] = PublicKey.findProgramAddressSync(
-        [
-            Buffer.from("reserve_coll_mint"),
-            lendingMarket.publicKey.toBuffer(),
-            mint.toBuffer(),
-        ],
-        klend.programId
-    );
-    const [reserveCollateralSupply] = PublicKey.findProgramAddressSync(
-        [
-            Buffer.from("reserve_coll_supply"),
-            lendingMarket.publicKey.toBuffer(),
-            mint.toBuffer(),
-        ],
-        klend.programId
-    );
-    return {
-        feeReceiver,
-        reserveLiquiditySupply,
-        reserveCollateralMint,
-        reserveCollateralSupply,
-    }
-}
+  const [feeReceiver] = PublicKey.findProgramAddressSync(
+    [
+      Buffer.from("fee_receiver"),
+      lendingMarket.publicKey.toBuffer(),
+      mint.toBuffer(),
+    ],
+    klend.programId
+  );
+  const [reserveLiquiditySupply] = PublicKey.findProgramAddressSync(
+    [
+      Buffer.from("reserve_liq_supply"),
+      lendingMarket.publicKey.toBuffer(),
+      mint.toBuffer(),
+    ],
+    klend.programId
+  );
+  const [reserveCollateralMint] = PublicKey.findProgramAddressSync(
+    [
+      Buffer.from("reserve_coll_mint"),
+      lendingMarket.publicKey.toBuffer(),
+      mint.toBuffer(),
+    ],
+    klend.programId
+  );
+  const [reserveCollateralSupply] = PublicKey.findProgramAddressSync(
+    [
+      Buffer.from("reserve_coll_supply"),
+      lendingMarket.publicKey.toBuffer(),
+      mint.toBuffer(),
+    ],
+    klend.programId
+  );
+  return {
+    feeReceiver,
+    reserveLiquiditySupply,
+    reserveCollateralMint,
+    reserveCollateralSupply,
+  };
+};
 
 export const userPDAs = (user: PublicKey) => {
-    const [bestlendUserAccount] = PublicKey.findProgramAddressSync(
-        [Buffer.from("bestlend_user_account"), user.toBuffer()],
-        program.programId
-    );
-    const [userMetadata] = PublicKey.findProgramAddressSync(
-        [Buffer.from("user_meta"), bestlendUserAccount.toBuffer()],
-        klend.programId
-    );
-    const [obligation] = PublicKey.findProgramAddressSync(
-        [
-            Uint8Array.from([0]),
-            Uint8Array.from([0]),
-            bestlendUserAccount.toBuffer(),
-            lendingMarket.publicKey.toBuffer(),
-            PublicKey.default.toBuffer(),
-            PublicKey.default.toBuffer(),
-        ],
-        klend.programId
-    );
-    return {
-        bestlendUserAccount,
-        userMetadata,
-        obligation
-    }
-}
+  const [bestlendUserAccount] = PublicKey.findProgramAddressSync(
+    [Buffer.from("bestlend_user_account"), user.toBuffer()],
+    program.programId
+  );
+  const [userMetadata] = PublicKey.findProgramAddressSync(
+    [Buffer.from("user_meta"), bestlendUserAccount.toBuffer()],
+    klend.programId
+  );
+  const [obligation] = PublicKey.findProgramAddressSync(
+    [
+      Uint8Array.from([0]),
+      Uint8Array.from([0]),
+      bestlendUserAccount.toBuffer(),
+      lendingMarket.publicKey.toBuffer(),
+      PublicKey.default.toBuffer(),
+      PublicKey.default.toBuffer(),
+    ],
+    klend.programId
+  );
+  return {
+    bestlendUserAccount,
+    userMetadata,
+    obligation,
+  };
+};
 
-const reserveAccounts = async (connection: Connection, user: Keypair, reserveKey: PublicKey, performer?: Keypair) => {
-    const reserve = await Reserve.fromAccountAddress(connection, reserveKey);
-    const payer = performer ?? user
+const reserveAccounts = async (
+  connection: Connection,
+  user: Keypair,
+  reserveKey: PublicKey,
+  performer?: Keypair
+) => {
+  const reserve = await Reserve.fromAccountAddress(connection, reserveKey);
+  const payer = performer ?? user;
 
-    const [bestlendUserAccount] = PublicKey.findProgramAddressSync(
-        [Buffer.from("bestlend_user_account"), user.publicKey.toBuffer()],
-        program.programId
-    );
+  const [bestlendUserAccount] = PublicKey.findProgramAddressSync(
+    [Buffer.from("bestlend_user_account"), user.publicKey.toBuffer()],
+    program.programId
+  );
 
-    // user account PDA ATAs
-    const collateralAta = await getOrCreateAssociatedTokenAccount(
-        connection,
-        payer,
-        reserve.collateral.mintPubkey,
-        bestlendUserAccount,
-        true
-    );
-    const liquidityAta = await getOrCreateAssociatedTokenAccount(
-        connection,
-        payer,
-        reserve.liquidity.mintPubkey,
-        bestlendUserAccount,
-        true
-    );
-    const userLiquidityAta = await getOrCreateAssociatedTokenAccount(
-        connection,
-        payer,
-        reserve.liquidity.mintPubkey,
-        payer.publicKey,
-        true
-    );
+  // user account PDA ATAs
+  const collateralAta = await getOrCreateAssociatedTokenAccount(
+    connection,
+    payer,
+    reserve.collateral.mintPubkey,
+    bestlendUserAccount,
+    true
+  );
+  const liquidityAta = await getOrCreateAssociatedTokenAccount(
+    connection,
+    payer,
+    reserve.liquidity.mintPubkey,
+    bestlendUserAccount,
+    true
+  );
+  const userLiquidityAta = await getOrCreateAssociatedTokenAccount(
+    connection,
+    payer,
+    reserve.liquidity.mintPubkey,
+    payer.publicKey,
+    true
+  );
 
-    const [obligation] = PublicKey.findProgramAddressSync(
-        [
-            Uint8Array.from([0]),
-            Uint8Array.from([0]),
-            bestlendUserAccount.toBuffer(),
-            lendingMarket.publicKey.toBuffer(),
-            PublicKey.default.toBuffer(),
-            PublicKey.default.toBuffer(),
-        ],
-        klend.programId
-    );
+  const [obligation] = PublicKey.findProgramAddressSync(
+    [
+      Uint8Array.from([0]),
+      Uint8Array.from([0]),
+      bestlendUserAccount.toBuffer(),
+      lendingMarket.publicKey.toBuffer(),
+      PublicKey.default.toBuffer(),
+      PublicKey.default.toBuffer(),
+    ],
+    klend.programId
+  );
 
-    return {
-        bestlendUserAccount,
-        reserve,
-        collateralAta,
-        liquidityAta,
-        userLiquidityAta,
-        obligation
-    }
-}
+  return {
+    bestlendUserAccount,
+    reserve,
+    collateralAta,
+    liquidityAta,
+    userLiquidityAta,
+    obligation,
+  };
+};
 
-const accountValueRemainingAccounts = async (connection: Connection, signer: Signer, user: PublicKey, mints: PublicKey[], oracles: PublicKey[]) => {
-    const [bestlendUserAccount] = PublicKey.findProgramAddressSync(
-        [Buffer.from("bestlend_user_account"), user.toBuffer()],
-        program.programId
-    );
+const accountValueRemainingAccounts = async (
+  connection: Connection,
+  signer: Signer,
+  user: PublicKey,
+  mints: PublicKey[],
+  oracles: PublicKey[]
+) => {
+  const [bestlendUserAccount] = PublicKey.findProgramAddressSync(
+    [Buffer.from("bestlend_user_account"), user.toBuffer()],
+    program.programId
+  );
 
-    const remainingAccounts: AccountMeta[] = []
-    for (let i = 0; i < oracles.length; i++) {
-        const { address } = await getOrCreateAssociatedTokenAccount(
-            connection,
-            signer,
-            mints[i],
-            bestlendUserAccount,
-            true,
-        );
-        remainingAccounts.push({
-            pubkey: oracles[i],
-            isSigner: false,
-            isWritable: false
-        })
-        remainingAccounts.push({
-            pubkey: address,
-            isSigner: false,
-            isWritable: false
-        })
-    }
+  const remainingAccounts: AccountMeta[] = [];
+  for (let i = 0; i < oracles.length; i++) {
+    const { address } = await getOrCreateAssociatedTokenAccount(
+      connection,
+      signer,
+      mints[i],
+      bestlendUserAccount,
+      true
+    );
+    remainingAccounts.push({
+      pubkey: oracles[i],
+      isSigner: false,
+      isWritable: false,
+    });
+    remainingAccounts.push({
+      pubkey: address,
+      isSigner: false,
+      isWritable: false,
+    });
+  }
 
-    return remainingAccounts
-}
+  return remainingAccounts;
+};
 
-const swapAccounts = async (connection: Connection, inputMint: PublicKey, outputMint: PublicKey, user: Keypair) => {
-    const [tokenPDA] = PublicKey.findProgramAddressSync(
-        [Buffer.from("token_holder")],
-        dummySwap.programId
-    );
-    const { address: inputATA } = await getOrCreateAssociatedTokenAccount(
-        connection,
-        user,
-        inputMint,
-        user.publicKey,
-        true,
-    );
-    const { address: outputATA } = await getOrCreateAssociatedTokenAccount(
-        connection,
-        user,
-        outputMint,
-        user.publicKey,
-        true,
-    );
-    const { address: pdaInputATA } = await getOrCreateAssociatedTokenAccount(
-        connection,
-        user,
-        inputMint,
-        tokenPDA,
-        true,
-    );
-    const { address: pdaOutputATA } = await getOrCreateAssociatedTokenAccount(
-        connection,
-        user,
-        outputMint,
-        tokenPDA,
-        true,
-    );
-    return {
-        tokenPDA,
-        inputATA,
-        outputATA,
-        pdaInputATA,
-        pdaOutputATA,
-    }
-}
+const swapAccounts = async (
+  connection: Connection,
+  inputMint: PublicKey,
+  outputMint: PublicKey,
+  user: Keypair
+) => {
+  const [tokenPDA] = PublicKey.findProgramAddressSync(
+    [Buffer.from("token_holder")],
+    dummySwap.programId
+  );
+  const { address: inputATA } = await getOrCreateAssociatedTokenAccount(
+    connection,
+    user,
+    inputMint,
+    user.publicKey,
+    true
+  );
+  const { address: outputATA } = await getOrCreateAssociatedTokenAccount(
+    connection,
+    user,
+    outputMint,
+    user.publicKey,
+    true
+  );
+  const { address: pdaInputATA } = await getOrCreateAssociatedTokenAccount(
+    connection,
+    user,
+    inputMint,
+    tokenPDA,
+    true
+  );
+  const { address: pdaOutputATA } = await getOrCreateAssociatedTokenAccount(
+    connection,
+    user,
+    outputMint,
+    tokenPDA,
+    true
+  );
+  return {
+    tokenPDA,
+    inputATA,
+    outputATA,
+    pdaInputATA,
+    pdaOutputATA,
+  };
+};
 
-export { lendingMarket, lendingMarketAuthority, reservePDAs, reserveAccounts, accountValueRemainingAccounts, swapAccounts }
+export {
+  lendingMarket,
+  lendingMarketAuthority,
+  reservePDAs,
+  reserveAccounts,
+  accountValueRemainingAccounts,
+  swapAccounts,
+};
